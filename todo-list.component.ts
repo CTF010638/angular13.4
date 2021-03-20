9c55db95cc615ffdda403d832b97401e45378818
CTF010638 <80992442+CTF010638@users.noreply.github.com>
2021-03-20 08:28:49 +0700

    Add files via upload

---
+++ todo-list.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent {
+ tasks: string[] = [];
+
+    task: string;
+    addTask(){
+        this.tasks.push(this.task);
+    }
+
+    deleteTask(taskIndex: number){
+        this.tasks = this.tasks.filter((t, index) => index !== taskIndex);
+    }
+
+
+}
